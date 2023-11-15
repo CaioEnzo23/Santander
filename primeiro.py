98b6d39fbb6fb3019d0f14093ffd7eaa3f5e7227
Caio Enzo <138871450+CaioEnzo23@users.noreply.github.com>
2023-11-15 08:59:07 -0300

    python 2

--- primeiro.py
+++ primeiro.py
@@ -1,1 +1,15 @@
+# variáveis
+
+idade = 24
+
+print(idade)
+
+nome = Neos
+
+print(Neos)
+
 print('Hello World!')
+
+linguagem = input('Qual a linguagem que você está estudando?')
+
+print('A linguagem que você está estudando é', linguagem)
